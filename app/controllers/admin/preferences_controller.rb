3add01453d469e6e3b41b1d08298f6944badb6fb
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 15:06:38 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,5 +1,5 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference=Preference.create(artist_sort_order:"DESC",song_sort_order:"ASC",allow_create_songs:true,allow_create_artists:true)
+    #@preference=Preference.create(artist_sort_order:"DESC",song_sort_order:"ASC",allow_create_songs:true,allow_create_artists:true)
   end
 end
